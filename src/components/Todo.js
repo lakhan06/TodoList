c1377d5a8b0f36d5a1ee93830723aa90e52d1af7
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-06-16 11:42:23 +0530

    css update

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,9 +8,10 @@ export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
     <div className="Todo">
         <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
         <div>
-        <div className="edit-icon"  onClick={() => editTodo(task.id)} >edit</div>
-        <div className="delete-icon"  onClick={() => deleteTodo(task.id)} />update</div>
+        <button className="edit-icon"  onClick={() => editTodo(task.id)} >edit</button>
+        <button className="delete-icon"  onClick={() => deleteTodo(task.id)} >update</button>
         
+    </div>
     </div>
   )
 }
